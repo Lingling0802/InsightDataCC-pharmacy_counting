f7ad64de9bd82fff45108abc42473a28fa87b79e
Lingling0802 <39530228+Lingling0802@users.noreply.github.com>
2018-07-16 22:07:28 -0400

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
+#
+
+#python3 ./src/pharmacy-counting.py ./own-test/input/own-input-for-itcont.txt ./own-test/output/top_cost_drug.txt
+
+python3 ./src/pharmacy-counting.py ./input/itcont.txt ./output/top_cost_drug.txt
+
+
